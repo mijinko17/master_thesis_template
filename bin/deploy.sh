a98a24949f8847559904a974fb6bd0495ceede2d
mijinko17 <kougouseikeidansi@gmail.com>
2020-02-17 16:25:33 +0900

    ファイル移動

--- bin/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-ln -s $SCRIPT_DIR/../cites.bib $SCRIPT_DIR/../src/cites.bib
-ln -s $SCRIPT_DIR/../.latexmkrc $SCRIPT_DIR/../src/.latexmkrc
-
-echo "cites.bib and .latexmkrc are deployed in ./src successfully!"
